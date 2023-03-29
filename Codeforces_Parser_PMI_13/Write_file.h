41696f1786d142cdab16ffa3739fe51fd3a1ffca
olejik-web <eremuchew.gelo@gmail.com>
2023-03-29 10:17:13 +0300

    add file write_file.h

---
+++ Codeforces_Parser_PMI_13/Write_file.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "Head.h"
+#inclue "Solution_stud.h"
+
+void write_info_stud(string& Name_student, vector<Solution_stud>& Stud) {
+    string File_info = folder_base_put + Folder_base_stud;
+    File_info += Name_student + '/';
+    File_info += Name_student + ".txt";
+    ofstream out(file_info);
+    for (int i=0; i<Stud.size(); i++) {
+        out << Stud[i].get_Name_contest << " ";
+        out << Stud[i].get_Name_problem << " ";
+        out << Stud[i].get_Name_package << " ";
+        out << Stud[i].get_verdict() << " ";
+        if (Stud[i].get_verdict() == "Coincidence") {
+            out << Stud[i].get_Name_stud_dist_levenstein() << " ";
+            out << Stud[i].get_Name_dist_levenstein_package() << " ";
+            out << Stud[i].get_dist_levenstein() << " ";
+        }
+        out << "\n";
+    }
+    out.close();
+}
+
+
+
+void write_info_all_stud(vector<pair<string, int> >All_stud) {
+    string file_info_all = folder_base_put + "result.txt";
+    ofstream out(file_info_all);
+    for (int i=0; i<All_stud.size(); i++) {
+        out << All_stud[i].first << " " << All_stud[i].second << "\n";
+    }
+    out.close();
+}
